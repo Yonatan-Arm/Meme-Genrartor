26db684dead89c6e3e1ff9134c232e4747250afc
Yonatan arm <yoniarm123@gmail.com>
2022-02-16 12:04:13 +0200

    start

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,23 @@
-console.log('hii');
+
+
+function init(){
+    document.querySelectorAll('img').forEach(item => {
+        item.addEventListener('click', event => {
+            onClickImg(item)
+        })
+      })
+}
+
+function onOpenMemeGenerator(){
+    renderMeme(memeId)
+}
+
+
+function onOpenGallery(){
+    var gallery=document.querySelector('.Image-Gallery')
+   
+}
+
+function onClickImg(img) {
+    img.style.border= "3px solid red";
+}
